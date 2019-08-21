8d199bc7bc81c4a55b8bec87618585d5da5fc652
rogerio48 <neresdeusdete04@gmail.com>
2019-08-21 11:57:14 -0300

    Atualizei as pastas Tipo Comercial

---
+++ BACKEND/TipoComecial/TipoComercialModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROPOSTA
+{
+    public partial class TipoComercial
+    {
+        private String Credential;
+        private String CurrentUser;
+        private SimLib clsLib = new SimLib();
+        public TipoComercial(String pCredential)
+        {
+            this.Credential = pCredential;
+            this.CurrentUser = clsLib.Decriptografa(clsLib.GetJsonItem(this.Credential, "Name"));
+        }
+
+        public class TipoComercialModel
+        {
+            public String Cod_Tipo_Comercial { get; set; }
+            public Boolean Absorcao { get; set; }
+            public String Descricao { get; set; }
+            public Boolean Roteiro_Tecnico { get; set; }
+            public Boolean Merchandising { get; set; }
+            public Boolean Cod_ADSolution { get; set; }
+            public Boolean Cod_Midia_Log { get; set; }
+            public Boolean INDICA_ABSORCAO_MERCHA { get; set; }
+            public Boolean Tipo_Valoracao { get; set; }
+            public Boolean Indica_Midia_On_Line { get; set; }
+            public String Exibidora_DAD { get; set; }
+            public Int32 Id_tipo_comercial { get; set; }
+            public Int32 Id_Assessor { get; set; }
+            public Byte Indica_Edicao_Assessor { get; set; }
+            public string id_operacao { get; set; }
+
+        }
+
+
+
+    }
+}
---
+++ FRONTEND/angular/Controller/TipoComercialController.js
@@ -0,0 +1,87 @@
+﻿angular.module('App').controller('TipoComercialController', ['$scope', '$rootScope', 'httpService', '$location', '$timeout', function ($scope, $rootScope, httpService, $location, $timeout) {
+    //========================Verifica Permissoes
+    $scope.PermissaoNew = false;
+    $scope.PermissaoEdit = false;
+    httpService.Get("credential/TipoComercial@New").then(function (response) {
+        $scope.PermissaoNew = response.data;
+    });
+    httpService.Get("credential/TipoComercial@Edit").then(function (response) {
+        $scope.PermissaoEdit = response.data;
+    });
+    //====================Inicializa scopes
+    $scope.ShowGrid = false;
+    $scope.gridheaders = [{ 'title': '', 'visible': true, 'searchable': false, 'sortable': false },
+    { 'title': 'Codigo', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Descricao', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': '.Absorcao', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'INDICA_ABSORCAO_MERCHA', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Roteiro_Tecnico', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Merchandising', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Tipo_Valoracao', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Indica_Midia_On_Line', 'visible': true, 'searchable': true, 'sortable': true },
+        { 'title': 'Exibidora_DAD', 'visible': true, 'searchable': true, 'sortable': true },
+    ];
+
+    //====================Quando terminar carga do grid, torna view do grid visible
+    $scope.RepeatFinished = function () {
+        $rootScope.routeloading = false;
+        $scope.ConfiguraGrid();
+        $scope.ShowGrid = true;
+        setTimeout(function () {
+            $("#dataTable").dataTable().fnAdjustColumnSizing();
+        }, 10)
+    };
+
+    //====================Carrega o Grid
+    $scope.CarregarTipoComercial = function () {
+        $rootScope.routeloading = true;
+        $scope.TipoComerciais = [];
+        $scope.ShowGrid = false;
+        $('#dataTable').dataTable().fnDestroy();
+        httpService.Get('TipoComercialListar').then(function (response) {
+            if (response) {
+                $scope.TipoComerciais = response.data;
+                if ($scope.TipoComerciais.length == 0) {
+                    $scope.RepeatFinished();
+                }
+            }
+        });
+    };
+    //====================Funcao para configurar o Grid
+    $scope.ConfiguraGrid = function () {
+        param = {};
+        param.language = fnDataTableLanguage();
+        param.lengthMenu = [[7, 10, 25, 50, -1], [7, 10, 25, 50, "Todos"]];
+        param.pageLength = 10;
+        param.scrollCollapse = true;
+        param.paging = true;
+
+        param.dom = "<'row'<'col-sm-3'l><'col-sm-4'f><'col-sm-5'B>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            param.buttons = [
+                {
+                    text: 'Exportar<span class="fa fa-file-excel-o margin-left-10" style="color:white"></span>', type: 'excel', className: 'btn btn-warning HideButton', extend: 'excel', exportOptions: {
+                        columns: ':visible:not(:first-child)'
+                    }
+                }
+            ];
+        param.order = [[0, 'asc']];
+        param.autoWidth = false;
+        param.columns = [];
+        for (var i = 0; i < $scope.gridheaders.length; i++) {
+            param.columns.push({ "visible": $scope.gridheaders[i].visible, "searchable": $scope.gridheaders[i].searchable, "sortable": $scope.gridheaders[i].sortable });
+        }
+        $('#dataTable').DataTable(param);
+    };
+    //===========================Evento chamado ao fim do ngrepeat ao carregar grid 
+    $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+        $scope.RepeatFinished();
+    });
+    //===========================fim do load da pagina
+    $scope.$watch('$viewContentLoaded', function () {
+        $scope.ConfiguraGrid();
+        $scope.CarregarTipoComercial();
+    });
+    $scope.EditarTipoComercial
+
+}]);
+
